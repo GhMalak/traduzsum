a34215b84e1066c52801670b82614f564c224a27
Gustavo Malaquias <gustavo.calasan@gmail.com>
2026-01-07 01:56:16 -0300

    Sistema de Login e Pagamento

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Garantir que as rotas de API sejam dinâmicas
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '2mb',
+    },
+  },
 }
 
 module.exports = nextConfig
